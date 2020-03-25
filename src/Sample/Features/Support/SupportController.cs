5f05e60d1c7a8faca0d7e2041d7ec57cbe74544f
Per Ökvist <perokvist@users.noreply.github.com>
2020-03-25 09:54:59 +0100

    cleanup

--- src/Sample/Features/Support/SupportController.cs
+++ src/Sample/Features/Support/SupportController.cs
@@ -1,9 +1,5 @@
-﻿using Fragments.Areas.Fragments.Models;
+﻿using Fragments;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Filters;
-using System;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Sample.Features.Support
 {
@@ -26,19 +22,5 @@ namespace Sample.Features.Support
         [HttpGet("Support.css.html")]
         [ResponseCache(Duration = 60 * 60)]
         public IActionResult SupportCss() => PartialView("Support.css");
-    }
-
-    public class FragmentAttribute : ActionFilterAttribute
-    {
-        public override Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
-        {
-            var name = (string)context.RouteData.Values["Controller"];
-            var f = context.HttpContext.RequestServices.GetService(typeof(FragmentModel[])) as FragmentModel[];
-            var r = f.Where(x => x.Id == name.ToLower()).FirstOrDefault();
-            context.HttpContext.Response.Headers.Add("x-fragment-html", r.Html);
-            context.HttpContext.Response.Headers.Add("x-fragment-css", r.Css);
-            context.HttpContext.Response.Headers.Add("x-fragment-js", r.Js);
-            return base.OnResultExecutionAsync(context, next);
-        }
     }
 }
